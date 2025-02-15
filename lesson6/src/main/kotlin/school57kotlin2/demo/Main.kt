d8f4f3d173a0bd60dadb7dcfae098960d65d77b3
alizalas <alice.laskina@yandex.ru>
2025-02-15 14:16:47 +0300

    Работа на уроке, 15.02.2025

--- lesson6/src/main/kotlin/school57kotlin2/demo/Main.kt
+++ lesson6/src/main/kotlin/school57kotlin2/demo/Main.kt
@@ -1,29 +1,94 @@
+//package school57kotlin2.demo
+//
+//import java.sql.DriverManager
+//import java.util.*
+//
+//
+//
+//fun main() {
+//
+//
+//    val url = "jdbc:postgresql://localhost:5432/postgres"
+//    val props = Properties().apply {
+//        setProperty("user", "baeldung")
+//        setProperty("password", "baeldung")
+//    }
+//
+//    DriverManager.getConnection(url, props).use { connection ->
+//        println(connection.metaData.databaseProductVersion)
+//
+//        connection.createStatement().use { statement ->
+//            val rs = statement.executeQuery("SELECT 1+1;")
+//
+//            rs.next()
+//            println(rs.getObject(1))
+//        }
+//    }
+//}
+//
+//
+
+
+
+
+
 package school57kotlin2.demo
 
 import java.sql.DriverManager
+import java.sql.Statement
 import java.util.*
 
-
-
 fun main() {
-
-
     val url = "jdbc:postgresql://localhost:5432/postgres"
     val props = Properties().apply {
-        setProperty("user", "baeldung")
-        setProperty("password", "baeldung")
+        setProperty("user", "myuser")
+        setProperty("password", "mypassword")
     }
 
-    DriverManager.getConnection(url, props).use { connection ->
-        println(connection.metaData.databaseProductVersion)
+    val connection = DriverManager.getConnection(url, props)
+    val statement: Statement = connection.createStatement()
 
-        connection.createStatement().use { statement ->
-            val rs = statement.executeQuery("SELECT 1+1;")
+    val createTableSQL = """
+        CREATE TABLE IF NOT EXISTS users
+        (
+          id SERIAL PRIMARY KEY,
+          username VARCHAR(255) NOT NULL UNIQUE,
+          password VARCHAR(255) NOT NULL,
+          email VARCHAR(255) UNIQUE,
+          firstname VARCHAR(255),
+          lastname VARCHAR(255),
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """.trimIndent()
 
-            rs.next()
-            println(rs.getObject(1))
-        }
+    statement.executeUpdate(createTableSQL)
+
+    val insertSQL = """
+        INSERT INTO users (username, password, email, firstname, lastname)
+        VALUES ('user_1', 'password', 'user@example.com', 'User', 'User')
+    """.trimIndent()
+
+    statement.executeUpdate(insertSQL)
+
+    val selectSQL = """
+        SELECT * FROM users WHERE username = 'john_doe'
+    """.trimIndent()
+
+    val resultSet = statement.executeQuery(selectSQL)
+
+    while (resultSet.next()) {
+        val id = resultSet.getInt("id")
+        val username = resultSet.getString("username")
+        val password = resultSet.getString("password")
+        val email = resultSet.getString("email")
+        val firstname = resultSet.getString("firstname")
+        val lastname = resultSet.getString("lastname")
+        val createdAt = resultSet.getTimestamp("created_at")
+
+        println("ID: $id, Username: $username, Password: $password, Email: $email, Firstname: $firstname, Lastname: $lastname, Created At: $createdAt")
     }
+
+    resultSet.close()
+    statement.close()
+    connection.close()
 }
-
-
--- lesson6/src/main/kotlin/school57kotlin2/demo/MyHibernate.kt
+++ lesson6/src/main/kotlin/school57kotlin2/demo/MyHibernate.kt
@@ -1,28 +1,144 @@
+//package school57kotlin2.demo
+//
+//import java.sql.Connection
+//
+//class MyRepository<T : Any>(
+//    val connection: Connection,
+//) {
+//
+//    init {
+//        //Тут можно проверить наличие таблицы
+//        TODO()
+//    }
+//
+//    inline fun <reified T : Any> create(obj: T): T {
+//        //return T::class.primaryConstructor!!.call(....)
+//        // Название таблички можно брать с названия класса
+//        TODO()
+//    }
+//
+//    inline fun <reified T : Any> read(id: Long): T {
+//        TODO()
+//    }
+//
+//    inline fun <reified T : Any> update(obj: T): T {
+//        TODO()
+//    }
+//
+//    inline fun <reified T : Any> delete(id: Long): T {
+//        TODO()
+//    }
+//
+//}
+//
+//fun main() {
+////    val connection: Connection = TODO()
+////    val repository = MyRepository<...>(connection)
+////
+////    val entity = repository.create(...)
+////    repository.read(entity.id)
+////        ..
+//}
+
+
+
+
 package school57kotlin2.demo
 
 import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.Statement
+import java.time.LocalDateTime
+import java.util.*
+import kotlin.random.Random
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.primaryConstructor
 
 class MyRepository<T : Any>(
     val connection: Connection,
+    val table: String,
 ) {
 
-    init {
-        //Тут можно проверить наличие таблицы
-        TODO()
-    }
-
     inline fun <reified T : Any> create(obj: T): T {
+        // Создаём список, где будем хранить атрибуты класса
+        val propertiesList = mutableListOf<Pair<String, String>>()
+
+        // Находим все поля в классе и итерируемся по ним
+        obj::class.declaredMemberProperties.forEach { properties ->
+            // Сохраняем имя и тип свойства в список
+            propertiesList.add(properties.name to properties.returnType.toString())
+
+            // Выводим в консоль название и тип поля
+            println("${properties.name} : ${properties.returnType}")
+        }
+
+        val statement: Statement = connection.createStatement()
+
+        val insertSQL = """
+        INSERT INTO ${table} (${propertiesList.map { it.first }.joinToString(", ")})
+        VALUES ('${propertiesList.map { it.second }.joinToString("', '")}')
+        """.trimIndent()
+
+        statement.executeUpdate(insertSQL)
+        connection.close()
+
+        return obj
         //return T::class.primaryConstructor!!.call(....)
-        // Название таблички можно брать с названия класса
-        TODO()
     }
 
     inline fun <reified T : Any> read(id: Long): T {
-        TODO()
+        val statement: Statement = connection.createStatement()
+
+        val selectSQL = """
+        SELECT * FROM ${table} WHERE id = ${id}
+        """.trimIndent()
+
+        val resultSet = statement.executeQuery(selectSQL)
+        connection.close()
+
+        val metaData = resultSet.metaData
+        val propertiesList = mutableListOf<Pair<String, Any>>()
+
+        for (i in 1..metaData.columnCount) {
+            propertiesList.add(metaData.getColumnName(i) to resultSet.getObject(i))
+        }
+
+        return T::class.primaryConstructor!!.call(propertiesList.map { it.second })
+
     }
 
     inline fun <reified T : Any> update(obj: T): T {
-        TODO()
+        // Создаём список, где будем хранить атрибуты класса
+        val propertiesList = mutableListOf<Pair<String, String>>()
+
+        // Находим все поля в классе и итерируемся по ним
+        obj::class.declaredMemberProperties.forEach { properties ->
+            // Сохраняем имя и тип свойства в список
+            propertiesList.add(properties.name to properties.returnType.toString())
+
+            // Выводим в консоль название и тип поля
+            println("${properties.name} : ${properties.returnType}")
+        }
+
+        val statement: Statement = connection.createStatement()
+
+        val selectSQL = """
+        SELECT id FROM ${table} 
+        WHERE ${propertiesList.map { (first, second) -> "$first = $second" }.joinToString(", ")}
+        """.trimIndent()
+
+        val resultSet = statement.executeQuery(selectSQL)
+
+        val insertSQL = """
+        INSERT INTO ${table} (${propertiesList.map { it.[0] }.joinToString(", ")})
+        VALUES ('${propertiesList.map { it.[1] }.joinToString("', '")}')
+        """.trimIndent()
+
+        statement.executeUpdate(insertSQL)
+        connection.close()
+
+        return obj
+        //return T::class.primaryConstructor!!.call(....)
     }
 
     inline fun <reified T : Any> delete(id: Long): T {
@@ -32,10 +148,17 @@ class MyRepository<T : Any>(
 }
 
 fun main() {
-//    val connection: Connection = TODO()
-//    val repository = MyRepository<...>(connection)
-//
-//    val entity = repository.create(...)
-//    repository.read(entity.id)
-//        ..
+    val url = "jdbc:postgresql://localhost:5432/postgres"
+    val props = Properties().apply {
+        setProperty("user", "myuser")
+        setProperty("password", "mypassword")
+    }
+
+    val connection = DriverManager.getConnection(url, props)
+    val connection: Connection = TODO()
+    val repository = MyRepository<...>(connection)
+
+    val entity = repository.create(...)
+    repository.read(entity.id)
+    ..
 }
